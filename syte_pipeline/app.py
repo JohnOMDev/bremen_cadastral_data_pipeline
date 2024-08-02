9fc2b638163d89a7c74e91dec07c1e3f974b855c
johnOMDev <omolejohna@gmail.com>
2024-08-02 23:18:28 +0200

    fix the app route

--- syte_pipeline/app.py
+++ syte_pipeline/app.py
@@ -2,9 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
-import uptrace
 from fastapi import FastAPI
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from starlette import status
 from starlette.responses import JSONResponse
 
@@ -12,7 +10,6 @@ from starlette.responses import JSONResponse
 import syte_pipeline
 from syte_pipeline.examples import v0_router
 from syte_pipeline.s1.analytic import v1
-from syte_pipeline.settings import Settings
 
 logger = logging.getLogger(__name__)
 
@@ -35,14 +32,7 @@ app = FastAPI(
     version=syte_pipeline.__version__,
 )
 
-uptrace.configure_opentelemetry(
-    # Copy DSN here or use UPTRACE_DSN env var.
-    dsn=Settings().telemetry_dsn,
-    service_name=syte_pipeline.__name__,
-    service_version=syte_pipeline.__version__,
-    logging_level=logging.INFO,
-)
-FastAPIInstrumentor.instrument_app(app)
+
 app.include_router(v0_router)
 app.include_router(v1)
 
